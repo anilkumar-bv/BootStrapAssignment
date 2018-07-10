db7a13e3791cff41dfce834dbcb57057af1e24a4
Anil Kumar <anilkumar_bv@yahoo.com>
2018-07-10 16:57:33 +0530

    Assignment on Bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browsersync = require('browser-sync').create();
+
+gulp.task('sass', () =>{
+    return gulp
+            .src(['src/scss/*.scss'])
+            .pipe(sass())
+            .pipe(gulp.dest('src/css/'))
+            .pipe(browsersync.stream())
+});
+
+gulp.task('js', () =>{
+    return gulp
+            .src([  'node_modules/bootstrap/dist/js/bootstrap.min.js', 
+                    'node_modules/jquery/dist/jquery.min.js',
+                    'node_modules/popper.js/dist/popper.min.js'])
+            .pipe(gulp.dest('src/js/'))
+            .pipe(browsersync.stream())
+});
+
+
+gulp.task('serve', ['sass'], () =>{
+    browsersync.init({
+        server:'./src'
+    });
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch('src/*.html').on('change', browsersync.reload);
+});
+
+gulp.task('default', ['serve', 'js']);
+
+
+
+
+
